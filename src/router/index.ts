0a0b9fb5b98ddcb3d243554def8de59fbb029c6b
NarcisNacev1 <nnaarrcciissnnaacceevv@gmail.com>
2025-11-22 21:57:31 +0100

    login design progress

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import Login from '@/views/Login.vue';
+
+const routes = [{ path: '/login', name: 'Login', component: Login }];
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: []
+    history: createWebHistory(),
+    routes
 });
 
 export default router;
